dddb41f2f0df8b1f30b229d1ab65d1ff051f2f64
quadra2003 <56703545+quadra2003@users.noreply.github.com>
2025-02-18 01:04:31 +0000

    rewrote eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,14 @@
-{
-    "extends": "next/core-web-vitals",
-    "rules": {
-      "@typescript-eslint/no-unused-vars": "off"
-    }
+// .eslintrc.js
+module.exports = {
+  root: true,
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'next/core-web-vitals'
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'off'
   }
+};
